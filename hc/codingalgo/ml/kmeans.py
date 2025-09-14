5581265fe494b0f57886494c547525cd2d9a6492
Haoyang Chen <haoychen@outlook.com>
2025-09-13 17:23:00 -0700

    Update kmeans.py

--- hc/codingalgo/ml/kmeans.py
+++ hc/codingalgo/ml/kmeans.py
@@ -59,3 +59,59 @@ def test_kmeans_simple_clusters():
     print(preds)
 
 test_kmeans_simple_clusters()
+
+
+
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+class KMeans(nn.Module):
+
+  def __init__(self, k: int, dim: int):
+    super().__init__()
+    self.k = k
+    self.centroids = nn.Parameter(torch.randn(k, dim)) # K, dim
+
+  def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+    distances = self.distance(inputs) # B, k
+    indices = distances.argmin(dim=1)
+    return indices
+
+  def distance(self, inputs: torch.Tensor) -> torch.Tensor:
+    sim = F.normalize(inputs, dim=1) @ F.normalize(self.centroids, dim=1).T
+    return 1 - sim
+
+  def loss(self, inputs: torch.Tensor) -> torch.Tensor:
+    distances = self.distance(inputs)
+    min_dist, _ = distances.min(dim=1)
+    return min_dist.mean()
+  
+
+  def train(k: int, data: torch.Tensor, epochs: int, batch_size: int):
+  n, dim = data.shape
+  model = KMeans(k, dim)
+  optimizer = optim.Adam(model.parameters(), lr=0.1)
+
+  for epoch in range(epochs):
+    perm = torch.randperm(n)
+    for i in range(0, n, batch_size):
+      optimizer.zero_grad()
+      batch = data[perm[i:i+batch_size]]
+      loss = model.loss(batch)
+      loss.backward()
+      optimizer.step()
+
+    print(f"epoch {epoch}, loss: {loss.item()}")
+    print(f"epoch {epoch}, centroids: {model.centroids}")
+
+  return model
+
+cluster1 = torch.randn(1000, 2) + torch.tensor([5.0, 5.0])
+cluster2 = torch.randn(1000, 2) + torch.tensor([-5.0, -5.0])
+x = torch.cat([cluster1, cluster2], dim = 0)
+
+model = train(2, x, 10, 100)
