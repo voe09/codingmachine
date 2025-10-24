b2d609bfc582b143940e59d177431869c58be5c2
Haoyang Chen <haoychen@outlook.com>
2025-10-23 21:53:22 -0700

    Update 278.py

--- hc/codingalgo/leetcode/278.py
+++ hc/codingalgo/leetcode/278.py
@@ -12,4 +12,56 @@ class Solution:
             else:
                 left = mid + 1
         return left
-        
+
+
+
+# Follow up
+class VersionControl:
+
+    def __init__(self, bad_version: str):
+        self.bad_version = tuple(int(v) for v in bad_version.split('.'))
+
+    def isBadVersion(self, version: str) -> bool:
+        version_num = tuple(int(v) for v in version.split('.'))
+        is_bad = version_num >= self.bad_version
+        return is_bad
+
+checker = VersionControl("2.1.9")
+
+def firstBadVersion(latest_version: str) -> str:
+    max_m, max_n, max_p = [int(v) for v in latest_version.split('.')]
+
+    left = 1
+    right = max_m
+    while left < right:
+        mid = left + (right - left) // 2
+        if checker.isBadVersion(f"{mid}.9.9"):
+            right = mid
+        else:
+            left = mid + 1
+    bad_m = left
+
+    left = 1
+    right = 9
+    while left < right:
+        mid = left + (right - left) // 2
+        if checker.isBadVersion(f"{bad_m}.{mid}.9"):
+            right = mid
+        else:
+            left = mid + 1
+    bad_n = left
+
+    left = 1
+    right = 9
+    while left < right:
+        mid = left + (right - left) // 2
+        if checker.isBadVersion(f"{bad_m}.{bad_n}.{mid}"):
+            right = mid
+        else:
+            left = mid + 1
+    bad_p = left
+
+    return f"{bad_m}.{bad_n}.{bad_p}"
+
+
+print(firstBadVersion("3.1.0"))
