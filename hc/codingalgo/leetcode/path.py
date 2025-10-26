9595fc77d3354296eb49440629c23111f9a74a8f
Haoyang Chen <haoychen@outlook.com>
2025-10-26 11:47:54 -0700

    Update path.py

--- hc/codingalgo/leetcode/path.py
+++ hc/codingalgo/leetcode/path.py
@@ -30,40 +30,86 @@ print(cd("/foo/bar", "../../../baz"))
 print(cd("/foo/bar", "~/foo/bar"))
 
 
-def cd_with_symlinks(
-    current_dir: str, 
-    new_dir: str, 
-    symlinks: dict[str, str],
-    home: str = "/home/usr",
-) -> str:
-    path = cd(current_dir, new_dir, home)
-    if path == "NULL":
-        return path
 
+from typing import Optional
+
+def simplify_path(path: str) -> str:
+    tokens = path.split("/")
+    stack = []
+    for token in tokens:
+        if token == "" or token == ".":
+            continue
+        elif token == "..":
+            if stack:
+                stack.pop()
+            else:
+                raise ValueError("Invalid Path")
+        stack.append(token)
+    return "/" + "/".join(stack)
+
+
+class TreeNode:
+
+    def __init__(self, token: str):
+        self.token = token
+        self.children = {}
+        self.path = None
+
+    def build(self, origin: str, new: str):
+        tokens = [t for t in origin.split("/") if t]
+        curr = self
+        for i in range(len(tokens)):
+            token = tokens[i]
+            if token not in curr.children:
+                curr.children[token] = TreeNode(token)
+            curr = curr.children[token]
+            if i == len(tokens) - 1:
+                curr.path = new
+    
+    def visit(self, path: str) -> Optional[tuple[str, str]]:
+        tokens = [t for t in path.split("/") if t]
+        curr = self
+        longest = None
+        visited = []
+
+        for token in tokens:
+            if token in curr.children:
+                visited.append(token)
+                curr = curr.children[token]
+                if curr.path:
+                    longest = ("/" + "/".join(visited), curr.path)
+            else:
+                break
+
+        return longest
+
+def cd(curr: str, new: str, links: dict[str, str]) -> str:
+    if new.startswith("/"):
+        path = simplify_path(new)
+    else:
+        path = simplify_path(curr + "/" + new)
+    
+    root = TreeNode("")
+    for k, v in links.items():
+        root.build(k, v)
+    
     visited = set()
     while True:
         if path in visited:
-            raise ValueError("circular dep")
+            raise ValueError("Circle detected")
 
         visited.add(path)
 
-        match = None
-        for k in symlinks:
-            if path == k or path.startswith(k + "/"):
-                if match is None or len(k) > len(match):
-                    match = k
-
+        match = root.visit(path)
         if match is None:
-            break
+            break        
         
-        target = symlinks[match]
-        path = path.replace(match, target, 1)
+        origin, replace = match
+        path = replace + path[len(origin):]
 
         path = simplify_path(path)
-        if path == "NULL":
-            return path
-    
+
     return path
 
-print("test symlinks")
-print(cd_with_symlinks("/foo/bar", "baz", {"/foo/bar": "/abc", "/foo/bar/baz": "/xyz"}))
+
+print(cd("/foo/bar", "baz", {"/foo/bar": "/abc", "/foo/bar/baz": "/xyz"}))
