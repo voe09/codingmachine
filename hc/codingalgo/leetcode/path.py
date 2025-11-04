4f28b18adba312b601758ac4429bca5fc0c42f91
Haoyang Chen <haoychen@outlook.com>
2025-11-03 20:24:51 -0800

    Update path.py

--- hc/codingalgo/leetcode/path.py
+++ hc/codingalgo/leetcode/path.py
@@ -162,3 +162,103 @@ class TestCD(unittest.TestCase):
         self.assertEqual("/haoyc", cd(self.root, "../..//haoyc"))
 
 unittest.main()
+
+
+
+
+
+class TreeNode:
+
+    def __init__(self, token: str):
+        self.token = token
+        self.children = {}
+        self.replace = None
+
+    def add(self, path: str, replace: str):
+        tokens = path.split("/")[1:]
+        curr = self
+        for i in range(len(tokens)):
+            token = tokens[i]
+            if token not in curr.children:
+                curr.children[token] = TreeNode(token)
+            curr = curr.children[token]
+            if i == len(tokens) - 1:
+                curr.replace = replace
+
+    def visit(self, path: str) -> tuple[str, bool]:
+        tokens = path.split("/")[1:]
+
+        curr = self
+        updated = False
+        pos = -1
+        new = None
+        for i in range(len(tokens)):
+            token = tokens[i]
+            if token not in curr.children:
+                break
+            curr = curr.children[token]
+            if curr.replace is not None:
+                pos = i
+                new = curr.replace
+                updated = True
+        
+        if updated:
+            new_path = path.replace("/" + "/".join(tokens[:pos+1]), new)
+            return (new_path, True)
+        return (path, False)
+
+
+def cd_with_symlink(curr: str, path: str, symlinks: dict[str, str]) -> str:
+    if not path.startswith("/"):
+        path = curr + "/" + path
+
+    root = TreeNode("")
+    for origin, replace in symlinks.items():
+        root.add(simplify(origin), simplify(replace))
+
+    path = simplify(path)
+    visited = set([path])
+    while True:
+        path, updated = root.visit(path)
+        if not updated:
+            break
+    
+        path = simplify(path)
+        if path in visited:
+            raise ValueError("Circle Detected")
+        visited.add(path)
+    
+    return path
+
+class TestCDWithSymlink(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.root = "/home/usr"
+    
+    def test_replace_once_successfully(self):
+        self.assertEqual("/usr/circle", 
+        cd_with_symlink(self.root, "haoyc/circle", {self.root + "/haoyc": "/usr"}))
+
+        # longer one is being taken
+        self.assertEqual("/usr/circle", 
+        cd_with_symlink(self.root, "haoyc/circle", {self.root + "/haoyc": "/usr", self.root: "/dummy"}))
+
+    def test_replace_3_times_successfully(self):
+        self.assertEqual("/app/circle", 
+        cd_with_symlink(self.root, "haoyc/circle", {
+            self.root + "/haoyc": "/usr", 
+            self.root: "/dummy",
+            "/usr/circle": "/usr/haoyc/app/circle",
+            "//usr/haoyc/app": "/app"
+        }))
+
+    def test_circular_dependency(self):
+        with self.assertRaises(ValueError):
+            cd_with_symlink(self.root, "haoyc/circle", {
+            self.root + "/haoyc": "/usr", 
+            self.root: "/dummy",
+            "/usr/circle": "/home/usr/haoyc/circle",
+            "//usr/haoyc/app": "/app"
+        })
+
+unittest.main()
